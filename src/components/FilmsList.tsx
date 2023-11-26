f5473005710f83a488e39b2bb310a031c58994ab
Yaroslav Marenkov <101563117+Flipty148@users.noreply.github.com>
2023-11-26 17:27:53 +0300

    change size

--- src/components/FilmsList.tsx
+++ src/components/FilmsList.tsx
@@ -20,7 +20,7 @@ export default function FilmList({films}: Props) {
                                     || film.original_name.toLowerCase().includes(filter.toLowerCase()));
     return (
         <ThemeProvider theme={darkTheme}>
-            <Container maxWidth={'sm'} sx={{mt: 2}}>
+            <Container maxWidth={'sm'} sx={{mt: 2, width:'1200px'}}>
                 <Typography variant="h5" component={'h3'} sx={{mb:2}}>Список фильмов</Typography>
                 <TextField label="Поиск" variant="outlined" sx={{mb: 2, width:'100%'}}
                 onChange={(e) => setFilter(e.target.value)}/>
