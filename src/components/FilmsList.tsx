db8a8547cdec4d61623f79404132ccf3373a82be
Yaroslav Marenkov <101563117+Flipty148@users.noreply.github.com>
2023-11-26 17:20:18 +0300

    add live search

--- src/components/FilmsList.tsx
+++ src/components/FilmsList.tsx
@@ -1,6 +1,8 @@
 import {Container, Stack, Typography, ThemeProvider, createTheme} from '@mui/material';
 import { Film } from '../types';
 import FilmComponent from './FilmComponent';
+import {TextField} from "@mui/material";
+import { useState } from 'react';
 
 type Props = {
     films: Film[];
@@ -13,12 +15,17 @@ const darkTheme = createTheme({
   });
 
 export default function FilmList({films}: Props) {
+    const [filter, setFilter] = useState('');
+    const filterFilms = films.filter((film) => film.russian_name.toLowerCase().includes(filter.toLowerCase())
+                                    || film.original_name.toLowerCase().includes(filter.toLowerCase()));
     return (
         <ThemeProvider theme={darkTheme}>
             <Container maxWidth={'sm'} sx={{mt: 2}}>
                 <Typography variant="h5" component={'h3'} sx={{mb:2}}>Список фильмов</Typography>
+                <TextField label="Поиск" variant="outlined" sx={{mb: 2, width:'100%'}}
+                onChange={(e) => setFilter(e.target.value)}/>
                 <Stack spacing={2}>
-                    {films.map((film) => (
+                    {filterFilms.map((film) => (
                         <FilmComponent key={film.id} film={film} />
                     ))}
                 </Stack>
